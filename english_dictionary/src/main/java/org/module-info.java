9ca03d84785b09900731419183d19acfecc774de
chitrung-gg <125481514+chitrung-gg@users.noreply.github.com>
2023-11-06 15:51:09 +0700

    Update module-info (still some bugs)

---
+++ english_dictionary/src/main/java/org/module-info.java
@@ -0,0 +1,16 @@
+// module english_dictionary {
+//     requires javafx.controls;
+//     requires javafx.fxml;
+//     requires java.sql;
+//     requires org.xerial.sqlitejdbc;
+//     requires javafx.web;
+//     requires javafx.graphics;
+//     requires javafx.base;
+//     requires javafx.media;
+//     requires com.jfoenix;
+//     opens com.sample to javafx.graphics;
+//     exports com.sample;
+//     opens org.controllers;
+//     opens org.features;
+//     opens org.main;
+// }
